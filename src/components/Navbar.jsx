8ef314efb15f42c4d30be86271865ab3f04be8bc
Ipan Badruzzaman <ipannbzn1@gmail.com>
2024-10-14 14:12:51 +0700

    Add mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,44 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { IoMdClose, IoMdMenu } from "react-icons/io";
 export default function Navbar() {
   return (
     <nav className="py-6">
       <div className="container flex justify-between items-center">
         <h1 className="font-bold text-3xl">Education.</h1>
-        <ul className="flex space-x-5 uppercase font-bold">
+        <ul className="hidden md:flex space-x-5 uppercase font-bold justify-center flex-1">
           <li>Beranda</li>
           <li>Materi</li>
           <li>Quiz Interaktive</li>
         </ul>
-        <a href="" className="py-0.5 px-1 rounded-sm bg-primary text-white">
-          Tentang Kami
-        </a>
+        <div>
+          <a
+            href=""
+            className="py-0.5 px-1 rounded-sm bg-primary text-white hidden md:inline-block"
+          >
+            Tentang Kami
+          </a>
+          <Menu>
+            <MenuButton className="text-3xl md:hidden">
+              <IoMdMenu />
+            </MenuButton>
+            <MenuItems
+              transition
+              className="bg-black/50 fixed inset-0 flex justify-center items-center p-8"
+            >
+              <MenuItem
+                as="button"
+                className="absolute top-1 right-6 text-white text-2xl"
+              >
+                <IoMdClose />
+              </MenuItem>
+              <ul className="bg-white rounded-lg w-full h-full px-4 py-3 space-y-4">
+                <MenuItem as={'li'}>Beranda</MenuItem>
+                <MenuItem as={'li'}>Materi</MenuItem>
+                <MenuItem as={'li'}>Quiz Interaktive</MenuItem>
+              </ul>
+            </MenuItems>
+          </Menu>
+        </div>
       </div>
     </nav>
   );
